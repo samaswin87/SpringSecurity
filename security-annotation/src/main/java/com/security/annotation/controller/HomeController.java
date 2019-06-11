4f285fd09ccf5ef0f5d9106710f3bb02d97c74f4
samaswin87 <aswin.r312@outlook.com>
2019-06-11 21:08:47 +0530

    added config project

---
+++ security-annotation/src/main/java/com/security/annotation/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.security.annotation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security");
+		model.addObject("message", "Welcome to spring security");
+		model.setViewName("home");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security");
+		model.addObject("message", "This is protected page - Admin Page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/user**", method = RequestMethod.GET)
+	public ModelAndView dbaPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page - User Page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+}
